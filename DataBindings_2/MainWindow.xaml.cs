a9d1cfa6df67b904451a374eb258a3f2ef40f6d5
MKA <mka@rivne.itstep.org>
2023-05-06 12:30:21 +0300

    Add project files.

---
+++ DataBindings_2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindings_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel model = new ViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = model;
+        }
+    }
+    class ViewModel
+    {
+        public User MyUser { get; set; }
+        public User MyUser2 { get; set; }
+        public ViewModel()
+        {
+            MyUser = new User()
+            {
+                Name = "Bob",
+                Email = "bob@gmail.com"
+            };
+            MyUser2 = new User()
+            {
+                Name = "Tom",
+                Email = "tom@gmail.com"
+            };
+        }
+    }
+}
---
+++ Notepad/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notepad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel model = new ViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = model;
+        }
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            model.DeleteSelectedContact();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+    class ViewModel
+    {
+        ObservableCollection<Contact> contacts = new ObservableCollection<Contact>();
+        public IEnumerable<Contact> Contacts => contacts;
+        public Contact SelectedContact { get; set; }
+        public ViewModel()
+        {
+            contacts.Add(new Contact() { Name = "Vova", Age = 30, Surname = "Pupkin", Phone = "+3807575828", IsMale = true });
+            contacts.Add(new Contact() { Name = "Marusia", Age = 25, Surname = "Shishik", Phone = "+380771244", IsMale = false });
+            contacts.Add(new Contact() { Name = "Olga", Age = 33, Surname = "Shelesh", Phone = "+38067285792", IsMale = false });
+        }
+        public void DeleteSelectedContact()
+        {
+            if (SelectedContact != null)
+                contacts.Remove(SelectedContact);
+        }
+    }
+    class Contact
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+        public bool IsMale { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} {Surname}";
+        }
+    }
+}
